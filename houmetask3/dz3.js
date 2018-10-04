b3f66d100357bc0e8a17123b1def26e03414bd39
vlkarasev46 <43551073+vlkarasev46@users.noreply.github.com>
2018-10-05 00:43:06 +0300

    Add files via upload

--- houmetask3/dz3.js
+++ houmetask3/dz3.js
@@ -1,7 +1,5 @@
 var today = new Date();
 
 var str = '';
-str += 23 - today.getHours();
-str += ':' ;
-str += 59 - today.getMinutes();
+str += 59 - today.getMinutes() + (23 - today.getHours())*60;;
 alert(str)
